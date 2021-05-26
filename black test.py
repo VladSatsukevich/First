4281a601842f729938a0f28db892ce5cd0dc3359
VladSatsukevich <84670334+VladSatsukevich@users.noreply.github.com>
2021-05-26 14:23:15 +0300

    Add files via upload

--- black test.py
+++ black test.py
@@ -1,32 +1,68 @@
 import random
 
-
-
 class BlackJack:
 
     def randomizer_dealer_cards():
         
-        card = ["Туз", "Король", "Дама","Валет", 6, 7, 8, 9, 10]*4
-        card_suits = ["♠", "♥", "♣", "♦"]
+        card = ['Туз', 'Король', 'Дама', 'Валет', 2, 3, 4, 5, 6, 7, 8, 9, 10]*4
+        card_suits = ['♠', '♥', '♣', '♦'] * 4
         first_card = random.choice(card)
         second_card = random.choice(card)
-        print(f'Карты диллера: {(first_card)}{random.choice(card_suits)} {second_card}{random.choice(card_suits)}')
+        res = (f'Карты диллера: {(first_card)}{random.choice(card_suits)} {second_card}{random.choice(card_suits)}')
+        print(res)
+        if first_card == 'Туз':
+            first_card = 1
+        elif second_card == 'Туз':
+            second_card = 1
+        elif first_card == 'Король':
+            first_card = 10
+        elif second_card == 'Король':
+            second_card = 10
+        elif first_card == 'Дама':
+            first_card = 10
+        elif second_card == 'Дама':
+            second_card = 10
+        elif first_card == 'Валет':
+            first_card = 10
+        elif second_card == 'Валет':
+            second_card = 10
+        print(f'Сумма карт диллера: {first_card + second_card}')
         
-        
-        print(f'Сумма карт диллера: {str(first_card) + str(second_card)}')
-    randomizer_dealer_cards()
-    
+rdc = BlackJack
+rdc.randomizer_dealer_cards()    
 
 
 class Player:
+
     def randomizer_player_cards():
-        card = ["Туз", "Король", "Дама","Валет", 6, 7, 8, 9, 10]
-        card_suits = ["♠", "♥", "♣", "♦"]
+
+        card = ["Туз", "Король", "Дама","Валет", 2, 3, 4, 5, 6, 7, 8, 9, 10]*4
+        card_suits = ["♠", "♥", "♣", "♦"]*4
         first_card = random.choice(card)
         second_card = random.choice(card)
-        return (f'{first_card}{random.choice(card_suits)} {second_card}{random.choice(card_suits)}')
-    print(f'Ваши карты: {randomizer_player_cards()}')
+        res = (f'Ваши карты: {first_card}{random.choice(card_suits)} {second_card}{random.choice(card_suits)}')
+        print(res)
+        if first_card == 'Туз':
+            first_card = 1
+        elif second_card == 'Туз':
+            second_card = 1
+        elif first_card == 'Король':
+            first_card = 10
+        elif second_card == 'Король':
+            second_card = 10
+        elif first_card == 'Дама':
+            first_card = 10
+        elif second_card == 'Дама':
+            second_card = 10
+        elif first_card == 'Валет':
+            first_card = 10
+        elif second_card == 'Валет':
+            second_card = 10
+        print(f'Сумма ваших карт: {first_card + second_card}')
 
+rpc = Player
+rpc.randomizer_player_cards()
     
+
    
 
